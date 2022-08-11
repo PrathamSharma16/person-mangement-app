054d182fe6619f8fd5b067395a6bd96d08c9423e
PrathamSharma16 <pratham0544.be20@chitkara.edu.in>
2022-08-11 20:03:39 +0530

    Fetched Data

--- ajax.js
+++ ajax.js
@@ -1,25 +1,28 @@
-const xhr = new XMLHttpRequest()
-console.log(`Ready State - ${xhr.readyState}`)
+var btn=document.getElementById("profile").addEventListener("click",getData);
+var div=document.getElementById("postsDiv");
+var con=0;
 
-const url = `https://jsonplaceholder.typicode.com/users`
+function getData(){
+    let xhr=new XMLHttpRequest();
+    xhr.open('GET','https://jsonplaceholder.typicode.com/users',true);
+    xhr.onload=function(){
 
-xhr.open('GET', url)
-console.log(`Ready State - ${xhr.readyState}`)
-
-console.log(c)
-xhr.onreadystatechange  = () => {
-    console.log(`Ready State - ${xhr.readyState}`)
-    if(xhr.readyState === 4 && xhr.status === 200) {
-        const objectResponse = JSON.parse( xhr.responseText )
-        console.log(objectResponse)
-        var output = ''
-        for(let i=0; i < objectResponse.length; i++) {
-                output += `
-                <img src=${objectResponse[i].id} />
+        if(this.status===200){
+            let data = JSON.parse(this.responseText);
+            
+            for(let index= 0;index<10;index++){
+                div.innerHTML+=`
+                    <div class="col-3 text-white card bg-dark mx-auto border border-white" >
+                    <div class="card-body" >
+                        <p class="card-title">ID: ${data[con].id}</p>
+                        <p class="card-title">Name: ${data[con].name}</p>
+                        <p >Email: ${data[con].email}</p>
+                    </div>
+                </div>
             `
+                con=con+1;
         }
-        console.log(output)
-        document.querySelector('#my-container').innerHTML = output
     }
 }
-xhr.send()
+xhr.send();
+}
